c867b3494ea1516d71e66854979be03f60a8f6cc
Chaewon Jeong <jcw3241@gmail.com>
2025-10-01 16:05:56 +0900

    init: tastelog spring boot project (with H2 profile)

---
+++ src/main/java/com/tastelog/tastelog/TastelogApplication.java
@@ -0,0 +1,13 @@
+package com.tastelog.tastelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tastelog/tastelog/TastelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tastelog.tastelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
