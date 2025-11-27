3dadf2d076b415b5776be3e4c13464e0893a8ffc
Chaewon Jeong <jcw3241@gmail.com>
2025-11-27 17:43:46 +0900

    user dto 파일 추가

---
+++ src/main/java/com/tastelog/user/dto/UserRegisterRequest.java
@@ -0,0 +1,56 @@
+package com.tastelog.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.UniqueElements;
+
+public class UserRegisterRequest {
+
+    @NotBlank
+    @Email
+    @Size(max = 100)
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
+    private String password;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UserRegisterRequest() {}
+
+    public UserRegisterRequest(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+
+}
